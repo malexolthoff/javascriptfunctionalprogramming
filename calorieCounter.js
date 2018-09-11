5f1bbc55f698aa918cf304c2c00dd677d0447c58
Michael Olthoff <m.alex.olthoff@gmail.com>
2018-09-11 22:20:04 +0800

    Functional programming

---
+++ calorieCounter.js
@@ -0,0 +1,16 @@
+const calories = 180;
+
+const calories2 = '180';
+
+
+const snack = {
+    calories: 189,
+    description: 'Snickers'
+}
+
+const updatedCalories = {
+    calories: snack.calories + 150,
+    description: snack.description
+}
+
+console.log(updatedCalories.calories)
