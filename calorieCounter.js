c5d028d5ec170865d7406f584a87c3ba84c74915
Michael Olthoff <m.alex.olthoff@gmail.com>
2018-09-11 22:39:17 +0800

    Some more function stuff

--- calorieCounter.js
+++ calorieCounter.js
@@ -13,4 +13,37 @@ const updatedCalories = {
     description: snack.description
 }
 
-console.log(updatedCalories.calories)
+const meals = [
+    { description: 'Breakfast', calories: 469},
+    { description: 'Lunch', calories: 500;}
+]
+
+function checkGrade(grade) {
+    if (grade >= 90) {
+        console.log('A');
+    }
+    else if (grade >= 80) {
+        console.log('B');
+    }
+    else if (grade >= 70) {
+        console.log('C');
+    }
+    else if (grade >= 60) {
+        console.log('D');
+    }
+    else {
+        console.log('F')
+    }
+}
+
+const name = 'James';
+function greet(greeting,nameinput) {
+  return greeting + ' ' + name
+}
+const greeting = greet('Good Day', name);
+
+
+console.log(greeting); 
+
+
+
